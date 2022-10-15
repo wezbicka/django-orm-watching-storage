5b79b48e68e1dcbff5153bdc05f882bb6bab5925
Polina <75391420+wezbicka@users.noreply.github.com>
2022-10-15 20:27:59 +0300

    Deleted comment

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -15,6 +15,6 @@ def storage_information_view(request):
         }
         non_closed_visits.append(visit)
     context = {
-        'non_closed_visits': non_closed_visits,  # не закрытые посещения
+        'non_closed_visits': non_closed_visits,
     }
     return render(request, 'storage_information.html', context)
